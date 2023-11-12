858c788925595687864fd81dbee8bcc62689d8f4
TitouanMAROTTA <titouan.marotta@gmail.com>
2023-11-12 16:01:41 +0100

    event update

---
+++ database/migrations/2023_11_11_165041_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('address');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('access');
+            $table->string('instagram_post_link')->nullable();
+            $table->string('facebook_post_link')->nullable();
+            $table->string('creator')->nullable();
+            $table->text('summary')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
